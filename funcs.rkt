749aa50af192451220889d1f1f36729846c03c5e
Konstantin Milchev <konstantin.milchev@agepartnership.com>
2022-09-10 20:05:21 +0100

    Added subs2 function

--- funcs.rkt
+++ funcs.rkt
@@ -64,3 +64,13 @@
 	  ((eq? (car lat) old) (cons new (cdr lat)))
 	  (else
 	    (cons (car lat) (subs new old (cdr lat)))))))))
+
+(define subs2
+  (lambda (new o1 o2 lat)
+    (cond
+    ((null? lat) '())
+    (else
+      (cond
+	((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+	(else
+	  (cons (car lat) (subs2 new o1 o2 (cdr lat)))))))))
--- temp.lisp
+++ temp.lisp
@@ -1,10 +1,9 @@
 (define subs2
-  (lambda (new old1 old2 lat)
+  (lambda (new o1 o2 lat)
     (cond
-      ((null? lat) '())
-      (else
-	(cond
-	  ((or (eq? (car lat) old1) 
-	  (eq? (car lat) old2)) (cons new (cdr lat)))
-	  (else
-	    (cons (car lat) (subs2 new old1 old2 (cdr lat)))))))))
+    ((null? lat) '())
+    (else
+      (cond
+	((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+	(else
+	  (cons (car lat) (subs2 new o1 o2 (cdr lat)))))))))
