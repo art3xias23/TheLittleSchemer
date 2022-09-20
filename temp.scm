741ea91c3997b3b65272ebe311c9a2d0198989d4
Konstantin Milchev <konstantin.milchev@agepartnership.com>
2022-09-20 20:46:03 +0100

    Added leftmost member

--- temp.scm
+++ temp.scm
@@ -1,40 +1,30 @@
-(define insertR*
-  (lambda (new old l)
-      (cond
-	((null? l) '())
-	((atom? (car l))
-	 (cond
-	   ((eq? (car l) old) (cons (car l) (cons new  (insertR* new old (cdr l)))))
-	   (else
-	     (cons (car l) (insertR* new old (cdr l))))))
-	(else
-	  (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
-
-(define occur*
-  (lambda(a l)
-    (cond
-      ((null? l) 0)
-      ((atom? (car l))
-       (cond
-	 ((eq? (car l) a) (add1 (occur* a (cdr l))))
-	 (else
-	   (occur* a (cdr l)))))
-      (else
-	(+ (occur* a (car l)) (occur* a (cdr l)))))))
-
-(define subst* 
-  (lambda (new old l)
+(define insertL*
+  (lambda(new old l)
     (cond
       ((null? l) '())
       ((atom? (car l))
        (cond
-	 ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+	 ((eq? (car l) old) (cons new (cons (car l) (insertL* new old (cdr l)))))
 	 (else
-	   (cons (car l) (subst* new old (cdr l))))))
+	   (cons (car l) (insertL* new old (cdr l))))))
       (else
-	(cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+	(cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
 
+(define member* 
+  (lambda (a l)
+    (cond
+      ((null? l) #f)
+      ((atom? (car l)) (or (eq? (car l) a) (member* a (cdr l)))) 
+      (else
+	(or (member* a (car l)) (member* a (cdr l)))))))
 
-'(("how much" '("wood")) "could" '('("a" '("wood") "chuck")) '('('("chuck"))) '("if" '("a") '('("wood chuck"))) "could chuck wood")
+(define leftmost
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l)) (car l))
+      (else
+	(leftmost (car l))))))
 
-'('("banana") '("split" '('('('("banana ice"))) '("cream" '("banana")) "sherbet")) '("banana") '("bread") '("banana brandy"))
+((potato) (chips ((with) fish) (chips)))
+
